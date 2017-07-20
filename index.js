b11166d9a128942eec76a5eb69ebbbb37f265e52
Manuele Cappelleri <manu@cappelleri.net>
2017-07-20 15:01:07 +0200

    OK

--- index.js
+++ index.js
@@ -333,12 +333,14 @@ function tryParse(p, str) {
           return new Date(value);
         }
       }
-      console.log('Server side?', __SERVER__);
+      // console.log('Server side?', __SERVER__);
+      /*
       if (__SERVER__ && typeof value === 'string' && (value.length === 24)) {
         if (ObjectId.isValid(value)) {
           return ObjectId(value);
         }
       }
+      */
 
       return value;
     }
